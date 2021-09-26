b06371e071a74b5ba4da9506f78644d0e8e0c25b
steve zhang <871506263@qq.com>
2021-09-27 00:38:27 +0800

    提交

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-import {hi}  from './lib'
+import { hi } from './lib';
 function sum1(a: number, b: number): number {
   return a + b;
 }
+
+const obj: { name: string; [key: string]: unknown } = { name: '孙悟空' };
+console.log('obj :>> ', obj);
+obj.age = 500;
+console.log('obj :>> ', obj);
+
 console.log('结果: ', sum1(11, 22));
 console.log('hi :>> ', hi);
+
+console.log('Promise :>> ', Promise);
--- webpack.config.js
+++ webpack.config.js
@@ -16,14 +16,14 @@ module.exports = {
     // 指定打包文件的目录
     path: path.resolve(rootDir, './dist'),
     // 打包后文件的文件
-    filename: 'bundle.js'
-    // 告诉webpack不使用箭头
+    filename: 'bundle.js',
+    // 告诉webpack不使用箭头(不实用箭头函数主要是为了兼容IE)
     // 默认打包后是一个立即执行的箭头函数，在IE 11中也是无法执行的！
     // 加上下面的配置，可以在webpack打包时，最外层不再是箭头函数
     // webpack新版本已经不想兼容IE了！233
-    // environment: {
-    //   arrowFunction: false
-    // }
+    environment: {
+      arrowFunction: false
+    }
   },
   // 指定webpack打包时要使用模块
   module: {
@@ -33,7 +33,37 @@ module.exports = {
         // test指定的是规则生效的文件
         test: /\.ts$/,
         // 要使用的loader: Webpack在加载时是"从后向前"加载
-        use: 'ts-loader',
+        use: [
+          //配置babel
+          {
+            // 指定加载器
+            loader: 'babel-loader',
+            //设置babel
+            options: {
+              // 设置预定义的环境
+              presets: [
+                // 预定义的第一个环境
+                [
+                  //指定环境插件
+                  '@babel/preset-env',
+                  // 配置信息
+                  {
+                    // 要兼容的目标浏览器
+                    targets: {
+                      chrome: '58',
+                      ie: '11'
+                    },
+                    //指定corejs的版本: package.json中的版本为3.8.1
+                    corejs: 3,
+                    // 使用corejs的方式，"usage" 表示按需加载
+                    useBuiltIns: 'usage'
+                  }
+                ]
+              ]
+            }
+          },
+          'ts-loader'
+        ],
         exclude: /node_modules/
       }
     ]
@@ -48,7 +78,7 @@ module.exports = {
   ],
   // 用来设置引用模块
   resolve: {
-    extensions: ['.ts','...']
+    extensions: ['.ts', '...']
   }
 };
 
