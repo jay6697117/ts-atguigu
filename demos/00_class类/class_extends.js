01ffcb9014ee95f25235bbf5bd8eff5abae1ea23
steve zhang <871506263@qq.com>
2021-11-28 23:33:01 +0800

    提交代码于: 2021-11-28 23:33:01

--- demos/00_class类/class_extends.js
+++ demos/00_class类/class_extends.js
@@ -1,3 +1,4 @@
+/*
 class Point {
   constructor(x, y) {
     this.x = x;
@@ -29,3 +30,40 @@ console.log(`cp:`, cp);
 console.log('-------------------------------');
 console.log(p.toString());
 console.log(cp.toString());
+*/
+
+
+/*
+class Point {
+  x = 1;
+  y = 2;
+}
+class ColorPoint extends Point {
+  constructor(...args) {
+    super(...args);
+  }
+}
+let p = new Point();
+let cp = new ColorPoint(); // ReferenceError
+console.log(`p:`, p);
+console.log(`cp:`, cp);
+*/
+
+
+/*
+function foo(...args) {
+  console.log(Object.prototype.toString.call(args));
+  console.log(`args:`, args);
+  args.forEach(element => {
+    console.log(`element:`, element);
+  });
+}
+foo(1, 2, 3);
+
+function baz(...arr) {
+  console.log(`arr`, arr);
+}
+
+const arr = [1, 2, 3];
+baz(...arr);
+*/
