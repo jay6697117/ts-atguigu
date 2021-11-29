4c337e4ef8bb0a37e55eb5b42d767a31a012872e
steve zhang <871506263@qq.com>
2021-11-29 11:00:06 +0800

    提交代码于: 2021-11-29 11:00:06

--- demos/00_class类/class_extends.js
+++ demos/00_class类/class_extends.js
@@ -32,7 +32,6 @@ console.log(p.toString());
 console.log(cp.toString());
 */
 
-
 /*
 class Point {
   x = 1;
@@ -48,7 +47,6 @@ let cp = new ColorPoint(); // ReferenceError
 console.log(`p:`, p);
 console.log(`cp:`, cp);
 */
-
 
 /*
 function foo(...args) {
@@ -66,4 +64,52 @@ function baz(...arr) {
 
 const arr = [1, 2, 3];
 baz(...arr);
+*/
+
+/*
+
+// 在子类的构造函数中，只有调用super之后，才可以使用this关键字，否则会报错。这是因为子类实例的构建，基于父类实例，只有super方法才能调用父类实例
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class ColorPoint extends Point {
+  constructor(x, y, color) {
+    // this.color = color; // Uncaught ReferenceError: Must call super constructor in derived class before accessing 'this' or returning from derived constructor
+    super(x, y);
+    this.color = color; // 正确
+  }
+}
+
+const cp  = new ColorPoint(1,2,'red');
+console.log(`cp:`, cp)
+
+console.log(cp instanceof ColorPoint) // true：爸爸
+console.log(cp instanceof Point) // true：爷爷
+
+*/
+
+
+/*
+
+class A {
+  static hello() {
+    console.log('hello world');
+  }
+}
+class B extends A {}
+console.dir(A);
+console.dir(B);
+B.hello(); // hello world
+
+const res = Object.getPrototypeOf(B);
+console.log(`res :`, res);
+console.log(`res === A :`, res === A);
+console.log(`B.__proto__ === A ;`, B.__proto__ === A);
+console.log(`new B() instanceof A :`, new B() instanceof A);
+
 */
