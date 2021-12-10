b0c9d1b137e6138ce9da3fd4592063978605ca4b
steve zhang <871506263@qq.com>
2021-12-10 17:20:23 +0800

    提交代码于: 2021-12-10 17:20:23

--- src/index.ts
+++ src/index.ts
@@ -9,23 +9,35 @@ class Person {
   static isHuman: boolean = true;
   //只读静态属性(类访问)
   // static readonly isHuman: boolean = true;
+  static name1: string = '类人名';
   // 构造方法
   constructor(name?: string, age?: number) {
     this.name = name ? name : this.name;
     this.age = age ? age : this.age;
   }
-  // 原型方法
+  // 实例方法
   sayHello() {
-    console.log(`hello, ${this.name}`);
+    console.log(`this:`, this);
+    console.log(`hello: ${this.name}`);
+  }
+  // 静态方法(类访问)
+  static sayHello1() {
+    console.log(`hello: ${this.name1}`);
   }
 }
 const p = new Person();
 const p1 = new Person('张金辉', 34);
 console.log(`p:`, p);
 console.log(`p1 000:`, p1);
-p1.name = '张金辉1';
+p1.name = '张金辉-6';
 console.log(`p1 001:`, p1);
 console.log(`Person.prototype:`, Person.prototype);
 console.log(`Person.isHuman 000:`, Person.isHuman);
 Person.isHuman = false;
 console.log(`Person.isHuman 001:`, Person.isHuman);
+p.sayHello();
+p1.sayHello();
+Person.sayHello1();
+console.log('-----------------------------');
+Person.prototype.sayHello();
+// Person.prototype.sayHello1(); //方法sayHello1在类 Person 上不存在
