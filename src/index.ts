ae84d8bfa3eb9ad056f9ba6b303dd2c3fc69e88a
steve zhang <871506263@qq.com>
2021-12-10 16:09:00 +0800

    提交代码于: 2021-12-10 16:09:00

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello world');
+// 1.0 类（class）
+class Person {
+  //属性
+  name: string;
+
+  // 构造方法
+  constructor(name: string) {
+    this.name = name;
+  }
+  // 方法
+  sayHello() {
+    console.log(`hello, ${this.name}`);
+  }
+}
+const p = new Person('zjh');
+console.log(`p:`, p);
+console.log(`Person.prototype:`, Person.prototype);
