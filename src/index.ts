d1604f62c26384f9b6162ca633a934234ce2e703
steve zhang <871506263@qq.com>
2021-12-11 23:02:38 +0800

    提交代码于: 2021-12-11 23:02:38

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@
     name: string;
     constructor(name: string) {
       this.name = name;
+      ``;
     }
     sayHello() {
+      console.log('我是父类sayHello');
       console.log(this.name + ' say hello!');
     }
   }
@@ -12,19 +14,24 @@
   class Dog extends Animal {
     age: number;
     constructor(name: string, age: number) {
-      super(name);
+      // 如果在子类中写了构造函数，在子类构造函数中必须对父类的构造函数进行调用
+      super(name);//调用父类的构造函数
       this.age = age;
-      super.sayHello();
     }
-    sayHello() {
-      console.log(this.name + ' 重写 say hello!');
+    sayHello1() {
+      // 在类的方法中 super就表示当前类的父类
+      console.log('我是子类sayHello');
+      super.sayHello(); //这里调的是父类的实例方法
+      // console.log(this.name + ' 重写 say hello!');
     }
   }
 
   const animal = new Animal('动物');
   console.log(`animal:`, animal);
   animal.sayHello();
+  console.log('--------------------------------');
   const dog = new Dog('旺财', 3);
   console.log(`dog:`, dog);
-  dog.sayHello();
+  // dog.sayHello();
+  dog.sayHello1();
 })();
