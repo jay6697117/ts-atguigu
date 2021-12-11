21d4186bf655a725b42628b922405b199a2bd89d
steve zhang <871506263@qq.com>
2021-12-11 22:09:35 +0800

    提交代码于: 2021-12-11 22:09:35

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+(function () {
+  class Animal {
+    name: string;
+    constructor(name: string) {
+      this.name = name;
+    }
+    sayHello() {
+      console.log(this.name + ' say hello!');
+    }
+  }
+
+  class Dog extends Animal {
+    age: number;
+    constructor(name: string, age: number) {
+      super(name);
+      this.age = age;
+    }
+    sayHello() {
+      console.log(this.name + ' 重写 say hello!');
+    }
+  }
+
+  const animal = new Animal('动物');
+  console.log(`animal:`, animal);
+  animal.sayHello();
+  const dog = new Dog('旺财', 3);
+  console.log(`dog:`, dog);
+  dog.sayHello();
+})();
