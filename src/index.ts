2adc1e5c139e94613a3b09c8cd141a72ebb8a390
steve zhang <871506263@qq.com>
2021-12-26 12:42:36 +0800

    提交代码于: 2021-12-26 12:42:36

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,119 @@
 (function () {
-  // 类型别名: 描述一个对象类型
-  type myType = {
-    name: string;
-    age: number;
-    [propName: string]: any;
-  };
-
-  /**
-   * 接口用来定义一个类结构，用来定义一个类中应该包含哪些属性和方法
-   * 同时接口也可以当成类型声明去使用
-   * 接口可以重复申明
-   */
-  interface myInterface {
-    name: string;
-    age: number;
-  }
-
-  interface myInterface {
-    [propName: string]: any;
-  }
-
-  const obj: myInterface = {
-    name: 'zjh',
-    age: 222,
-    gender: '男'
-  };
-  console.log(`obj:`, obj);
-
+  console.log('属性的封装');
+  // TS可以在属性前添加属性的修饰符
   /*
-   * 接口可以在定义类的时候去限制类的结构，
-   *   接口中的所有的属性都不能有实际的值
-   *   接口只定义对象的结构，而不考虑实际值
-   *       在接口中所有的方法都是抽象方法
+   *   public 修饰的属性可以在任意位置访问（修改） 默认值
+   *   private 私有属性，私有属性只能在类内部进行访问（修改）
+   *       - 通过在类中添加方法使得私有属性可以被外部访问
+   *   protected 受保护的属性，只能在当前类和当前类的子类中访问（修改）
    *
    * */
-  interface myInter {
-    name: string;
-    sayHello(): void;
+  class Person {
+    private _name: string;
+    private _age: number;
+
+    constructor(name: string, age: number) {
+      this._name = name;
+      this._age = age;
+    }
+
+    /*
+     *   getter方法用来读取属性
+     *   setter方法用来设置属性
+     *       - 它们被称为属性的存取器
+     * */
+    // getName() {
+    //   return this.name;
+    // }
+    // setName(newVal: string) {
+    //   this.name = newVal;
+    // }
+    // getAge() {
+    //   return this.age;
+    // }
+    // setAge(newVal: number) {
+    //   if (newVal < 0) {
+    //     alert('年龄参数格式不对');
+    //     return;
+    //   }
+    //   this.age = newVal;
+    // }
+
+    // TS中设置getter方法的方式
+    get name() {
+      return this._name;
+    }
+
+    set name(newVal) {
+      this._name = newVal;
+    }
+
+    get age() {
+      return this._age;
+    }
+
+    set age(newVal) {
+      if (newVal < 0) {
+        console.error('set age:', '年龄参数格式不对');
+        return;
+      }
+      this._age = newVal;
+    }
   }
 
   /*
-   * 定义类时，可以使类去实现一个接口,
-   *   实现接口就是使类满足接口的要求
-   *   接口和抽象类区别:
-   *   1.抽象类中可以定义抽象方法也可以定义非抽象方法，在接口中所有的方法都是抽象方法
-   *   2.抽象类是专门用来被继承的类，使用extends关键字，而接口是implements(实现)这个关键字
+   * 现在属性是在对象中设置的，属性可以任意的被修改,
+   *   属性可以任意被修改将会导致对象中的数据变得非常不安全
    * */
-  class MyClass implements myInter {
-    name: string;
-    constructor(name: string) {
-      this.name = name;
-    }
-    sayHello(): void {
-      console.log('大家好~~');
+  const p = new Person('zjh', 33);
+  console.log(`p 1:`, p);
+  p.name = 'ytt'; // 报错
+  p.age = -32; // 报错
+  // console.log(`p.getName() 1:`, p.getName());
+  // console.log(`p.getAge() 1:`, p.getAge());
+  // p.setName('ytt');
+  // p.setAge(-32);
+  console.log(`p 2:`, p);
+  // console.log(`p.getName() 2:`, p.getName());
+  // console.log(`p.getAge() 2:`, p.getAge());
+
+  console.log('---------------------------------------------');
+  // class A {
+  //   // public _age: number;
+  //   // private _age: number;
+  //   protected _age: number;
+  //   constructor(age: number) {
+  //     this._age = age;
+  //   }
+  //   printAge1() {
+  //     console.log(`printAge this._age:`, this._age);
+  //   }
+  // }
+
+  class A {
+    // protected _age: number;
+    // constructor(_age: number) {
+    //   this._age = _age;
+    // }
+    constructor(protected _age: number) {}
+    printAge1() {
+      console.log(`printAge1:`, this._age);
     }
   }
-  const m = new MyClass('初始值');
-  console.log(`m:`, m);
-  m.sayHello();
+
+  const a = new A(100);
+  console.log(`a`, a);
+  // console.log(`a._age:`, a._age);
+  a.printAge1();
+
+  class B extends A {
+    printAge2() {
+      console.log(`printAge2:`, this._age);
+    }
+  }
+
+  const b = new B(100);
+  console.log(`b`, b);
+  // console.log(`b._age:`, b._age);
+  b.printAge2();
 })();
